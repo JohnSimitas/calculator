3f8e225fc16417a3da43693c103a6aa3c8509595
John Simitas <johnsimitas06@gmail.com>
2022-12-15 22:14:05 +0200

    Recreating the whole calculator infrastructure

--- script.py
+++ script.py
@@ -3,60 +3,48 @@ from operators import mathmetical_operators
 def calc():
     while True:
         print("""
-            ------------------------
             |Available Operators    |
-            |-----------------------|
+            |                       |
             |1) Addition +          |
-            |-----------------------|
+            |                       |
             |2) Subtraction -       |
-            |-----------------------|
+            |                       |
             |3) Division /          |
-            |-----------------------|
+            |                       |
             |4) Multiplication *    |
-            |-----------------------|
+            |                       |
             |5) Power **            |
-            |-----------------------|
+            |                       |
+            |6) Sqrt                |
         """)
 
         choice = input('Choose one of the available operators: ')
 
-        while choice != '+' and choice != '-' and choice != '*' and choice != '/' and choice != '*' and choice != '**':
+        while choice != '+' and choice != '-' and choice != '*' and choice != '/' and choice != '*' and choice != '**' and choice != 'Sqrt':
             add_again = input(f'The operator {choice} is not supported add one of the available operatos: ')
 
-            if add_again == '+' or add_again == '-' or add_again == '*' or add_again == '/' or add_again == '**':
+            if add_again == '+' or add_again == '-' or add_again == '*' or add_again == '/' or add_again == '**' or add_again == 'Sqrt':
                 break
         
-        if choice == '**':
-            num = int(input('Add a number: '))
-            expo = int(input('Add the exposure: '))
-            
-            mathmetical_operators.power(num, expo)
-            
-            another_calc = input('Do you want to continue on other calculations? ')
-
-            if another_calc == 'no':
-                break
-            else:
-                continue
-
-        num = int(input('Add the 1st number: '))
-        num_2 = int(input('Add the 2nd number: '))
-
+        
         if choice == '+':
-            mathmetical_operators.addition(num, num_2)
+            mathmetical_operators.addition()
         elif choice == '-':
-            mathmetical_operators.subtraction(num, num_2)
+            mathmetical_operators.subtraction()
         elif choice == '*':
-            mathmetical_operators.multiplication(num, num_2)
+            mathmetical_operators.multiplication()
+        elif choice == '/':
+            mathmetical_operators.division()
+        elif choice == '**':
+            mathmetical_operators.power()
         else:
-            mathmetical_operators.division(num, num_2)
+            mathmetical_operators.square_root()
 
-        another_calc = input('Do you want to do anoter calculation? ')
-        
+        another_calc = input('Do you want to make another calculation yes / no? ')
+
         if another_calc == 'yes':
             continue
         else:
             break
-
 
 calc()
